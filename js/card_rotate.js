45b448829a96cbd1f78ddaa4cde0aa69725c198b
SerGorban <80527309+SerGorban@users.noreply.github.com>
2022-04-25 00:16:58 +0300

    Created a card rotate effect for contacts + perspective inner elements

--- js/card_rotate.js
+++ js/card_rotate.js
@@ -4,22 +4,34 @@ const cards = document.querySelectorAll('.card-rotate');
 
 if (cards) {
     for (let card of cards) {
+        
+        card.addEventListener('mouseover', () => {
+            const cardСhildren = card.children;
+            //console.log(cardСhildren);
+            for (let innerElement of cardСhildren) {
+                if (innerElement.hasAttribute('data-perspective-element-coords')) {
+                    const coords = innerElement.getAttribute('data-perspective-element-coords').split(',');
+                    innerElement.style.transform = `translate3d(${coords[0]}px, ${coords[1]}px, ${coords[2]}px)`;
+                    card.style.transformStyle = 'preserve-3d';
+                }
+            } 
+        }); 
+        card.addEventListener('mouseout', cardBack);  
         card.addEventListener('mousemove', cardRotate);
-        card.addEventListener('mouseout', cardBack);
 
         function cardRotate() {
-            let cardcoord = card.getBoundingClientRect();
-            let x = (event.pageX - cardcoord.x - (cardcoord.width / 2)) / 7;
-            let y = (event.pageY - cardcoord.y - (cardcoord.height / 2)) / 7;
-            console.log(x, y);
-            card.style.transform = `rotateY(${x}deg) rotateX(${y}deg) scale(1.05)`;
-            card.style.transition = 'none'
+            const coefficient = card.getAttribute('data-rotate-coefficient');
+            let cardСoord = card.getBoundingClientRect();
+            let x = (event.pageX - cardСoord.x - (cardСoord.width / 2)) / coefficient;
+            let y = (event.pageY - cardСoord.y - (cardСoord.height / 2)) / coefficient;
+            card.style.transform = `rotateY(${x}deg) rotateX(${-y}deg) scale(1.05)`;
+            card.style.transition = 'none';
         }
-
+        
         function cardBack() {
             card.style.transform = `rotateY(0deg) rotateX(0deg) scale(1)`;
             card.style.transition = '0.6s all 0s';
+            card.style.transformStyle = 'none';
         }
     }
 }
-
